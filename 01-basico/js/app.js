c244effa498a35617f42915ae46d2818af03766f
hnegrete <hector.negrete21@gmail.com>
2022-06-20 23:43:17 -0500

    Conclusión de usuarios falsos

--- 01-basico/js/app.js
+++ 01-basico/js/app.js
@@ -4,15 +4,26 @@ const load = async () => {
   const data = await d3.json('https://randomuser.me/api?results=10')
   //console.log(data.results);
 
-  data.results.forEach((r) => {
-    console.log(r.name.title, r.name.first, r.name.last);
-  })
+  //data.results.forEach((r) => {
+  //  console.log(r.name.title, r.name.first, r.name.last);
+  //})
+  
+  const rows = registros.selectAll('tr').data(data.results)
+  rows.enter().append('tr').html(r => `<td>
+  <img src="${r.picture.medium}">
+</td>
+<td>
+  <h3>${r.name.title}. ${r.name.first} ${r.name.last}</h3>
+  <p>${r.location.street.number} ${r.location.street.name}, ${r.location.city}<br>${r.location.country}</p>
+</td>
+`)
 }
 
 load()
 
 {
-/* <tr>
+/*
+<tr>
 <td>
     <img src="https://randomuser.me/api/portraits/med/men/54.jpg" class="rounded-circle">
 </td>
