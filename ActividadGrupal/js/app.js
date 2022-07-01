aa0a7b749af76f18f7c6dea6a820e1766afd447e
hnegrete <hector.negrete21@gmail.com>
2022-06-30 23:43:20 -0500

    Visualización

--- ActividadGrupal/js/app.js
+++ ActividadGrupal/js/app.js
@@ -1,6 +1,7 @@
 // Selecciones
 const metrica1 = d3.select("#filtro1")
 const metrica2 = d3.select("#filtro2")
+const figura = d3.selectAll("#mapa")
 
 const margins = {
   top: 60,
@@ -10,7 +11,7 @@ const margins = {
 }
 
 // Formatos
-//let format = d3.format("0,000");
+const sepMiles = d3.format(",")
 
 // Filtros
 const opciones1 = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]
@@ -155,7 +156,7 @@ const drawHorizontal = async (el, filtro, titGraf, variable1 = "2015", variable2
     .duration(2500)
     .attr("x", (d) => x(xAccessor(d)))
     .attr("y", (d) => y(yAccessor(d)) + y.bandwidth()*0.75)
-    .text(xAccessor)
+    .text((d) => sepMiles(xAccessor(d)))
   
   // Títulos
   titulo.text(titGraf + (filtro == 1 ? variable : variable2))
@@ -169,6 +170,7 @@ const drawHorizontal = async (el, filtro, titGraf, variable1 = "2015", variable2
   // Eventos
   metrica1.on("change", (e) => {
     e.preventDefault()
+    drawMapa(e.target.value)
     drawVertical(el = "#graf1", filtro = 1, titGraf = "Mes de más robo, ", variable1 = e.target.value, variable2 = metrica2.property("value"))
     drawHorizontal(el = "#graf3", filtro = 1, titGraf = "Entidad más violenta, ", variable1 = e.target.value, variable2 = metrica2.property("value"))
   })
@@ -240,7 +242,7 @@ const drawVertical = async (el, filtro, titGraf, variable1 = "2015", variable2 =
     .attr("y", -15)
     .classed("titulo", true)
   
-  const etiquetas = g.append("g")
+  const etiquetas = g.append("g").classed("etiqdatos", true)
 
   // Ejes
   const xAxisGroup = g
@@ -256,10 +258,12 @@ const drawVertical = async (el, filtro, titGraf, variable1 = "2015", variable2 =
 
   // Accesores
   const yAccessor = (d) => d[variable]
-  data.sort((a, b) => yAccessor(b) - yAccessor(a))
+  if (el == "#graf4") {
+    data.sort((a, b) => yAccessor(b) - yAccessor(a))
+  }
 
   // Escaladores
-  y.domain([0, d3.max(data, yAccessor)])
+  y.domain([0, d3.max(data, yAccessor)*1.1])
   x.domain(d3.map(data, xAccessor))
   
   // Rectángulos (Elementos)
@@ -293,28 +297,32 @@ const drawVertical = async (el, filtro, titGraf, variable1 = "2015", variable2 =
   et
     .enter()
     .append("text")
-    .attr("x", (d) => x(xAccessor(d)) + x.bandwidth() / 2)
+    .attr("x", (d) => x(xAccessor(d)))
     .attr("y", (d) => y(0))
     .merge(et)
     .transition()
     .duration(2500)
-    .attr("x", (d) => x(xAccessor(d)) + x.bandwidth() / 2)
-    .attr("y", (d) => y(yAccessor(d)))
-    .text(yAccessor)
+    .attr("x", (d) => x(xAccessor(d)))
+    .attr("y", (d) => y(yAccessor(d))*0.95)
+    .text((d) => sepMiles(yAccessor(d)))
   
   // Títulos
   titulo.text(titGraf + (filtro == 1 ? variable : variable2))
   
   // Ejes
-  const xAxis = d3.axisBottom(x).ticks(8)
-  const yAxis = d3.axisLeft(y)
-  xAxisGroup.transition().duration(2500).call(xAxis)
+  const xAxis = d3.axisBottom(x)
+  const yAxis = d3.axisLeft(y).ticks(8)
   yAxisGroup.transition().duration(2500).call(yAxis)
-
+  if (el == "#graf4") {
+    xAxisGroup.transition().duration(2500).call(xAxis).selectAll("text").style("text-anchor", "end").attr("dx", "-.8em").attr("dy", ".15em").attr("transform","rotate(-20)")
+  } else{
+    xAxisGroup.transition().duration(2500).call(xAxis)
+  }
 
   // Eventos
   metrica1.on("change", (e) => {
     e.preventDefault()
+    drawMapa(e.target.value)
     drawVertical(el = "#graf1", filtro = 1, titGraf = "Mes de más robo, ", variable1 = e.target.value, variable2 = metrica2.property("value"))
     drawHorizontal(el = "#graf3", filtro = 1, titGraf = "Entidad más violenta, ", variable1 = e.target.value, variable2 = metrica2.property("value"))
   })
@@ -326,6 +334,76 @@ const drawVertical = async (el, filtro, titGraf, variable1 = "2015", variable2 =
   
 }
 
+// ---------------------------- Mapa ----------------------------
+const drawMapa = async (anio = "2015") => {
+  // Lectura de datos
+  const catEntidades = await d3.json("data/entidades.json")
+  const data = await d3.csv("data/datasetMapa.csv", d3.autoType)
+
+  // Dimensiones
+  const anchoTotal = +figura.style("width").slice(0,-2)
+  const altoTotal = anchoTotal
+
+  const ancho = anchoTotal
+  const alto = altoTotal
+
+  // Colores para el mapa
+  const color = d3
+    .scaleOrdinal()
+    .domain(opciones1)
+    .range(d3.schemeTableau10)
+  
+  // Elementos gráficos (layers)
+  d3.select("#mapa").selectAll("svg").remove();
+  const svg = figura
+    .append("svg")
+    .attr("width", anchoTotal)
+    .attr("height", altoTotal)
+
+  // Grupo con la gráfica principal
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(0, 100)`)
+  
+  // Título
+  const titulo = g
+    .append("text")
+    .attr("x", ancho / 2)
+    .attr("y", -50)
+    .classed("titulo", true)
+  
+  const mapa = g
+    .selectAll("svg")
+    .data(catEntidades)
+    .enter()
+    .append("svg:path")
+    .attr("transform", "scale(0.75)")
+    .attr("fill", "#581845")
+    //.transition()
+    //.duration(2500)
+    .attr("d", (d) => d.d)
+    .attr("entidad", (d) => d.entidad)
+    .attr("id", (d) => d.id)
+    .attr("fill", (d) => color(d[anio][0]))//"#581845")
+    .attr("fill-opacity", "0.8")
+    .on("mouseover", function(d) {
+      d3.select(this).attr("fill-opacity", "1")//.transition().duration(2500).attr("transform", "rotate(5)")
+    })
+    .on("mouseout", function(d) {
+      d3.select(this).attr("fill-opacity", "0.8")
+    })
+    .on("click", function(d) {
+      console.log(d3.select(this).attr("id"))
+      drawVertical(el = "#graf2", filtro = 2, titGraf = "Año más violento, ", variable1=metrica1.property("value"), variable2=d3.select(this).attr("entidad"))
+      drawVertical(el = "#graf4", filtro = 2, titGraf = "Los 8 delitos de mayor violencia, ", variable1=metrica1.property("value"), variable2=d3.select(this).attr("entidad"))
+    })
+
+  // Títulos
+  titulo.text("Tasa de homicidios por Entidad Federativa, " + anio)
+
+}
+
+drawMapa()
 drawVertical(el = "#graf1", filtro = 1, titGraf = "Mes de más robo, ")
 drawVertical(el = "#graf2", filtro = 2, titGraf = "Año más violento, ")
 drawVertical(el = "#graf4", filtro = 2, titGraf = "Los 8 delitos de mayor violencia, ")
